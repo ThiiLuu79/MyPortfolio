6568264e769f70fe44dd7367f4190d000edfbc69
ThiiLuu79 <the-minh.luong.1@ens.etsmtl.ca>
2022-07-25 12:55:02 -0400

    Compressing showDropdown function

--- js/index.js
+++ js/index.js
@@ -16,14 +16,7 @@ function showDropdown(dropdownId, showClass) {
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains(showClass)) {
-          openDropdown.classList.remove(showClass);
-        }
-      }
+      $('.dropdown-content').removeClass(showClass);
     }
   }
 }
