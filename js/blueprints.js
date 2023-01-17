8397e1968cd017a6fee8608bc6588b99513be517
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2023-01-17 11:24:25 -0500

    @

--- js/blueprints.js
+++ js/blueprints.js
@@ -55,11 +55,11 @@ class MyNavbar extends HTMLElement {
               </button>
             </div>
             <div id = "myDropdown" class="dropdown-content">
+              <a href="${project5.url}">${project5.name}</a>
               <a href="${project1.url}">${project1.name}</a>
               <a href="${project2.url}">${project2.name}</a>
               <a href="${project3.url}">${project3.name}</a>
               <a href="${project4.url}">${project4.name}</a>
-              <a href="${project5.url}">${project5.name}</a>
             </div>
             <li class = "nav__item" ><a href = "index.html#education" class="nav__link">Education</a></li>
             <li class = "nav__item" ><a href = "#contact" class="nav__link">Contact</a></li>
