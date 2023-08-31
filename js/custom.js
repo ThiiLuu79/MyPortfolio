6164fae203b4b5044804d32458ea8024267454ad
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2023-08-31 19:34:11 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -114,4 +114,4 @@ const ciussDateEnd = new Date(2019, 8);
 const inlibroDateStart = new Date(2022, 0);
 const inlibroDateEnd = new Date(2022, 3);
 const nbcDateStart = new Date(2023, 4, 14);
-const nbcDateEnd = new Date();
+const nbcDateEnd = new Date(2023, 8, 31);
